30ca89dd2eec298138a015e9ed56a7757194aeeb
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-02-14 19:41:18 -0500

    set up app shared components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import Nav from './shared/Nav'
+import { Routes }  from './routes'
+import Footer from './shared/Footer'
 
-function App() {
-  return (
+
+class App extends React.Component {
+
+  render () {
+    return (
     <div className="App">
-      <h1>hello</h1>
+      <header>
+        <Nav/>
+      </header>
+      <main>
+        <Routes/>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
+  }
+  
 }
 
 export default App;
---
+++ src/shared/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+class Nav extends React.Component {
+    render () {
+        return (
+            <nav>
+                <h1>this is the nav</h1>
+            </nav>
+        )
+    }
+}
+
+export default Nav
